508d3b2cf2d86637de48d2084424147afd07f3b3
backinfile <1757518497@qq.com>
2024-08-04 22:06:22 +0800

    update

--- src/main/java/ShoujoKagekiNana/relics/ShineRelic.java
+++ src/main/java/ShoujoKagekiNana/relics/ShineRelic.java
@@ -1,7 +1,10 @@
 package ShoujoKagekiNana.relics;
 
 import ShoujoKagekiCore.base.BaseRelic;
+import ShoujoKagekiCore.shine.DisposableVariable;
 import ShoujoKagekiNana.ModPath;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
@@ -9,13 +12,35 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class ShineRelic extends BaseRelic {
     public static final String ID = ModPath.makeID(ShineRelic.class.getSimpleName());
+    private boolean canTrigger = false;
 
     public ShineRelic() {
         super(ID, RelicTier.STARTER, LandingSound.FLAT);
     }
 
     @Override
+    public void atBattleStart() {
+        super.atBattleStart();
+        beginLongPulse();
+        canTrigger = true;
+    }
+
+    @Override
+    public void onVictory() {
+        super.onVictory();
+        stopPulse();
+        canTrigger = false;
+    }
+
+    @Override
     public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
         super.onUseCard(targetCard, useCardAction);
+
+        if (DisposableVariable.isDisposableCard(targetCard) && canTrigger) {
+            canTrigger = false;
+            stopPulse();
+            addToBot(new GainEnergyAction(1));
+            addToBot(new DrawCardAction(1));
+        }
     }
 }
