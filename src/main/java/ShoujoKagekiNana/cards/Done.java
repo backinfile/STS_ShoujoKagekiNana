40b6d567489a6d446cdd51b460c53cf6f8c97e12
backinfile <1757518497@qq.com>
2024-08-05 15:05:21 +0800

    add card

---
+++ src/main/java/ShoujoKagekiNana/cards/Done.java
@@ -0,0 +1,35 @@
+package ShoujoKagekiNana.cards;
+
+import ShoujoKagekiNana.ModPath;
+import ShoujoKagekiNana.actions.StageCardPowerUpAction;
+import ShoujoKagekiNana.actions.StageDiscoveryAction;
+import ShoujoKagekiNana.modifiers.AddBlockModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Done extends BaseCard {
+    public static final String ID = ModPath.makeID(Done.class.getSimpleName());
+
+    public Done() {
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        this.baseBlock = this.block = 12;
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new StageCardPowerUpAction(c -> {
+            CardModifierManager.addModifier(c, new AddBlockModifier());
+        }));
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            upgradeBlock(4);
+            initializeDescription();
+        }
+    }
+}
